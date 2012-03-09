ca9e4ebfdea78b6a00f84a2b91b53a86ea16d57a
Mack McCauley <mack@swiftsuremarketing.com>
2012-03-09 15:28:05 -0800

    fix date in res widget

--- themes/earthbox/template.php
+++ themes/earthbox/template.php
@@ -2,7 +2,10 @@
 
 function earthbox_preprocess_html(&$variables){
 	
-  drupal_add_js("http://www.webrezpro.com/Javascript/embed_bookingrequest.js?hotel_id=584&date_format=mm-dd-yy&font_style=font-size:10pt&format=portrait&max_adults=40&max_child_age=12&max_children=40&num_adults=2&access_code=on");
+  drupal_add_js("http://www.webrezpro.com/Javascript/embed_bookingrequest.js?hotel_id=584&date_format=mm-dd-yy&format=portrait&max_adults=40&max_child_age=4&max_children=40&num_adults=2&access_code=Password");
+  
+ /*   drupal_add_js("http://www.webrezpro.com/Javascript/embed_bookingrequest.js?hotel_id=584&date_format=mm-dd-yy&font_style=font-size:10pt&format=portrait&max_adults=40&max_child_age=12&max_children=40&num_adults=2&access_code=on"); 
+ */
   
 }
   
