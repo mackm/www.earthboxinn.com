f272f48b9b3010050620dd53813c6bd0afb3f6bc
mackm <mackm@rockisland.com>
2014-05-05 10:19:56 -0700

    add analytics linking

--- themes/earthbox/scripts.js
+++ themes/earthbox/scripts.js
@@ -6,15 +6,34 @@ function EmbedBookingRequest_OnLoad2()
 								    
 	}
 
-
+function StuffGACookieInForm(context)
+{
+	// Grab the analytics cooke and stuff it in the hidden form field
+	ga(function(tracker) {
+		window.mmlinkerParam = tracker.get('linkerParam');
+	});
+	jQuery("#gacookie").attr("value",window.mmlinkerParam);
+	
+	// Load the link decorator plugin while we are here 
+    ga('require', 'linker');
+    ga('linker:autoLink', ['reservation.worldweb.com']);
+}
 
 (function ($) {
 
   Drupal.behaviors.webres = {
-
+	
     attach :  function (context, settings) {
 		EmbedBookingRequest_OnLoad2() 
 	}
   }
+  
+  
+  Drupal.behaviors.gaimmediate = {
+	  attach: function (context, settings) {
+		  StuffGACookieInForm(context);
+    }
+  }
+  
 
 })(jQuery);
