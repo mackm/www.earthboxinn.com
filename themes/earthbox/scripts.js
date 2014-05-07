8b46e919dbeea21971a771b833e541dc4e2b7097
mackm <mackm@rockisland.com>
2014-05-07 10:38:43 -0700

    add on click

--- themes/earthbox/scripts.js
+++ themes/earthbox/scripts.js
@@ -8,17 +8,12 @@ function EmbedBookingRequest_OnLoad2()
 
 function StuffGACookieInForm(context)
 {   
-
 	// Grab the analytics cooke and stuff it in the hidden form field
 	ga(function(tracker) {
 		window.mmlinkerParam = tracker.get('linkerParam');
 	});
 	
-	jQuery("#gacookie").attr("value",window.mmlinkerParam);
-	// Load the link decorator plugin while we are here 
-    ga('require', 'linker');
-    ga('linker:autoLink', ['worldweb.com', 'webrezpro.com', 'webrez.com']);
-	
+	jQuery("#gacookie").attr("value",window.mmlinkerParam);	
 }
 
 
@@ -35,8 +30,18 @@ function StuffGACookieInForm(context)
   Drupal.behaviors.gaimmediate = {
 	  attach: function (context, settings) {
 		  StuffGACookieInForm(context);
+		  ga('require', 'linker');
+          ga('linker:autoLink', ['worldweb.com', 'webrezpro.com', 'webrez.com']);
     }
   }
+  
+  Drupal.behaviors.gaclick = {
+	 attach: function (context, settings) {
+     $('#edit-submit', context).click(function () {
+			StuffGACookieInForm('main');
+      });
+    }
+	};
   
 
 })(jQuery);
